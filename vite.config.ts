496abe52001a6ab91fbb729fa9f561ebd8ec4caa
shravangv <shravangv2@gmail.com>
2026-05-01 11:56:04 +0530

    added base

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/Surya_portfolio1/',
   plugins: [react()],
   
   optimizeDeps: {
