9dc86e689999f03fe7d280e5607f5c8d6a228ece
shravangv <shravangv2@gmail.com>
2026-06-07 09:28:22 +0530

    Changed ViteConfig

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/Surya_portfolio1/',
   plugins: [react()],
-  
+
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
